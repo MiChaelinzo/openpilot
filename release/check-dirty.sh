d25693cd3bbbee285cc0a19b1d998cef128e963e
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2023-01-26 17:14:25 +0300

    Update check-dirty.sh
    Optimization:
    You can use "set -euxo pipefail" instead of "set -e" to make the script more robust. This will make the script exit if any command exits with a non-zero status, print each command before executing, and exit if any variable is unset.
    You can change if [ ! -z "$(git status --porcelain)" ]; to if ! git diff-index --quiet HEAD --; which is more efficient and idiomatic way to check for a dirty working tree.
    New Feature:
    You can add a feature to check for the latest commit message and display it.
    You can add a feature to check if there are any untracked files and display them.
    You can add a feature to check if there are any uncommitted changes and ask user to commit or stash them before continuing

--- release/check-dirty.sh
+++ release/check-dirty.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/bash
-set -e
+set -euxo pipefail
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
-if [ ! -z "$(git status --porcelain)" ]; then
+if ! git diff-index --quiet HEAD --; then
   echo "Dirty working tree after build:"
   git status --porcelain
-  exit 1
+  read -p "You have uncommitted changes, would you like to commit or stash them before continuing? (c/s)" choice
+  case "$choice" in
+    c) git commit -m "Committed changes before continuing";;
+    s) git stash;;
+    *) echo "Invalid choice";;
+  esac
+fi
+
+echo "Latest commit message: $(git log -1 --pretty=%B)"
+if [ -n "$(git ls-files --others --exclude-standard)" ]; then
+  echo "Untracked files:"
+  git ls-files --others --exclude-standard
 fi
